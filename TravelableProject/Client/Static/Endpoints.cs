60dab7ed735fe5a133745f30d6c74a767a2de3d3
DOMINIC CHAN JIN KANG 2005053J <2005053J@student.tp.edu.sg>
2022-02-09 23:31:35 +0800

    lastupdate

--- TravelableProject/Client/Static/Endpoints.cs
+++ TravelableProject/Client/Static/Endpoints.cs
@@ -9,11 +9,11 @@ namespace TravelableProject.Client.Static
     {
         private static readonly string Prefix = "api";
 
-        public static readonly string RoomsEndpoint = $"{Prefix}/rooms";
-        public static readonly string DurationsEndpoint = $"{Prefix}/durations";
-        public static readonly string HotelsEndpoint = $"{Prefix}/hotels";
-        public static readonly string PaymentsEndpoint = $"{Prefix}/payments";
-        public static readonly string BookingsEndpoint = $"{Prefix}/bookings";
-        public static readonly string CustomersEndpoint = $"{Prefix}/customers";
+        public static readonly string RoomsEndpoint = $"{Prefix}/Rooms";
+        public static readonly string DurationsEndpoint = $"{Prefix}/Durations";
+        public static readonly string HotelsEndpoint = $"{Prefix}/Hotels";
+        public static readonly string PaymentsEndpoint = $"{Prefix}/Payments";
+        public static readonly string BookingsEndpoint = $"{Prefix}/Bookings";
+        public static readonly string CustomersEndpoint = $"{Prefix}/Customers";
     }
 }
